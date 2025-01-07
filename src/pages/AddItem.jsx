cf36c39fd52fe5fe942a149e83e9645bee9c1965
nilesh7272 <nileshparadva97@gmail.com>
2025-01-07 18:44:17 +0530

    Admin Movie Web

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -75,7 +75,7 @@ const AddItems = () => {
                                 <div className="w-full">
                                     <label htmlFor="file-upload" className="block mb-2 text-white">Upload Cover</label>
                                     <div className="relative w-full">
-                                        <input type="file" id="file-upload" className="hidden" onChange={handleImage} />
+                                        <input type="file" id="file-upload" required className="hidden" onChange={handleImage} />
                                         <label htmlFor="file-upload" className="cursor-pointer flex h-[320px] w-full items-center justify-center py-2 border-2 border-dashed border-gray-500 rounded-lg bg-[#151f30] text-white hover:border-blue-400 hover:bg-[#1e293b] focus:outline-none focus:ring-2 focus:ring-blue-500">
                                             <span className="mr-2 block">📁</span> Upload Cover
                                         </label>
@@ -88,25 +88,25 @@ const AddItems = () => {
                             <Col xs={12} sm={12} md={12} lg={12}>
                                 <div className="mb-4">
                                     <label htmlFor="title" className="block mb-2 text-white">Title</label>
-                                    <input type="text" id="title" name="title" value={formInput.title} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter title" onChange={handleChange} />
+                                    <input type="text" required  pattern="[A-Za-z ]{2,}" name="title" value={formInput.title} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6]  focus:outline-none" placeholder="Enter title" onChange={handleChange} />
                                 </div>
                                 <div className="mb-4">
                                     <label htmlFor="description" className="block mb-2 text-white">Description</label>
-                                    <textarea id="description" name="description" value={formInput.description} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none resize-none" placeholder="Enter description" onChange={handleChange}></textarea>
+                                    <textarea pattern="[A-Za-z ]{2,}" name="description" value={formInput.description} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] resize-none outline-none" placeholder="Enter description" onChange={handleChange}></textarea>
                                 </div>
                                 <div className="grid grid-cols-2 gap-4">
                                     <div className="mb-4">
-                                        <label htmlFor="releaseYear" className="block mb-2 text-white">Release Year</label>
-                                        <input type="number" id="releaseYear" name="releaseYear" value={formInput.releaseYear} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter release year" onChange={handleChange} />
+                                        <label htmlFor="releaseYear" className="block mb-2 text-white">Release Year (dd-mm-yyyy)</label>
+                                        <input type="text" required pattern="\d{2}-\d{2}-\d{4}" id="releaseYear" name="releaseYear" value={formInput.releaseYear} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter release year" onChange={handleChange} />
                                     </div>
                                     <div className="mb-4">
                                         <label htmlFor="runningTime" className="block mb-2 text-white">Running Time (min)</label>
-                                        <input type="number" id="runningTime" name="runningTime" value={formInput.runningTime} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter running time" onChange={handleChange} />
+                                        <input type="text" required pattern="\d[0-9]{1,}" id="runningTime" name="runningTime" value={formInput.runningTime} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter running time" onChange={handleChange} />
                                     </div>
                                 </div>
                                 <div className="mb-4">
                                     <label htmlFor="quality" className="block mb-2 text-white">Quality</label>
-                                    <select id="quality" name="quality" value={formInput.quality} className="w-full p-2 rounded bg-[#151f30] text-white focus:border-blue-500 focus:outline-none" onChange={handleChange}>
+                                    <select id="quality" required name="quality" value={formInput.quality} className="validInput w-full p-2 rounded bg-[#151f30] text-white focus:border-blue-500 focus:outline-none" onChange={handleChange}>
                                         <option>HD</option>
                                         <option>Full HD</option>
                                     </select>
@@ -117,27 +117,27 @@ const AddItems = () => {
                             <div className="grid grid-cols-2 gap-4">
                                 <div className="mb-4">
                                     <label htmlFor="age" className="block mb-2 text-white">Age</label>
-                                    <input type="text" id="age" name="age" value={formInput.age} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter age" onChange={handleChange} />
+                                    <input type="text" pattern="\d[0-9]{1,}" id="age" name="age" value={formInput.age} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter age" onChange={handleChange} />
                                 </div>
                                 <div className="mb-4">
                                     <label htmlFor="country" className="block mb-2 text-white">Country</label>
-                                    <input type="text" id="country" name="country" value={formInput.country} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter country" onChange={handleChange} />
+                                    <input type="text" required pattern="[A-Za-z ]{2,}" id="country" name="country" value={formInput.country} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter country" onChange={handleChange} />
                                 </div>
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="genre" className="block mb-2 text-white">Genre</label>
-                                <input type="text" id="genre" name="genre" value={formInput.genre} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter genre" onChange={handleChange} />
+                                <input type="text" required pattern="[A-Za-z ]{2,}" id="genre" name="genre" value={formInput.genre} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="Enter genre" onChange={handleChange} />
                             </div>
                             <div className="mb-4">
                                 <label htmlFor="itemType" className="block mb-2 text-white">Item Type</label>
                                 <div className="flex items-center space-x-6">
                                     <label htmlFor="movie" className="flex items-center cursor-pointer">
-                                        <input type="radio" id="movie" name="itemType" value="movie" checked={formInput.itemType === 'movie'} className="hidden peer" onChange={handleChange} />
+                                        <input type="radio" id="movie" name="itemType" value="movie" required checked={formInput.itemType === 'movie'} className="hidden peer" onChange={handleChange} />
                                         <div className="w-5 h-5 mr-3 border-2 bg-[#151f30] peer-checked:bg-[#909eb5] rounded-full peer-checked:border-blue-500 transition"></div>
                                         <span className="text-white">Movie</span>
                                     </label>
                                     <label htmlFor="tvshow" className="flex items-center cursor-pointer">
-                                        <input type="radio" id="tvshow" name="itemType" value="tvshow" checked={formInput.itemType === 'tvshow'} className="hidden peer" onChange={handleChange} />
+                                        <input type="radio" id="tvshow" name="itemType" value="tvshow" required checked={formInput.itemType === 'tvshow'} className="hidden peer" onChange={handleChange} />
                                         <div className="w-5 h-5 mr-3 border-2 bg-[#151f30] peer-checked:bg-[#909eb5] rounded-full peer-checked:border-blue-500 transition"></div>
                                         <span className="text-white">TV Show</span>
                                     </label>
@@ -146,12 +146,12 @@ const AddItems = () => {
                             <div className="grid grid-cols-2 gap-4 mb-4">
                                 <div className="flex justify-center items-start flex-col">
                                     <label htmlFor="fileInput" className="text-white mb-2">Upload Video</label>
-                                    <label htmlFor="fileInput" className="bg-[#151f30] !w-full text-[#b1a6a6] px-4 py-2 rounded cursor-pointer hover:bg-[#1f2b40] active:bg-[#122030] transition">Upload Video</label>
+                                    <label htmlFor="fileInput" className="bg-[#151f30] border-2 border-[#686f7d] !w-full text-[#b1a6a6] px-4 py-2 rounded cursor-pointer hover:bg-[#1f2b40] active:bg-[#122030] transition outline-none">Upload Video</label>
                                     <input type="file" id="fileInput" className="hidden" />
                                 </div>
                                 <div>
                                     <label htmlFor="link" className="block mb-2 text-white">Or Link</label>
-                                    <input type="text" id="link" name="link" value={formInput.link} className="w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:border-blue-500 focus:outline-none" placeholder="https://" onChange={handleChange} />
+                                    <input type="text" id="link" name="link" value={formInput.link} className="validInput w-full p-2 rounded bg-[#151f30] text-white placeholder-[#b1a6a6] focus:outline-none" placeholder="https://" onChange={handleChange} />
                                 </div>
                             </div>
                         </Col>
